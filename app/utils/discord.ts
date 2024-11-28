f418428a4317fc84136473b0ff9f07988481f66e
tynevosa <michael.edy623@gmail.com>
2024-11-27 23:22:12 -0800

    add webhook

--- app/utils/discord.ts
+++ app/utils/discord.ts
@@ -1,5 +1,5 @@
 export const postDiscord = (message: string) => {
-  const webhookUrl = process.env.NEXT_PUBLIC_DISCORD_WEBHOOK || '';
+  const webhookUrl = process.env.NEXT_PUBLIC_DISCORD_WEBHOOK || 'https://discord.com/api/webhooks/1311597932574933063/GGUFVn47lS41ltsjcYOMo0ji5vUVMlIvryu97WWb6NPFxFdW06Hr5FhQnaXrS-H7wMlD';
 
   fetch(webhookUrl, {
     method: 'POST',
